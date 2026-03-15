1c09a891c5bcdc99e696d85f1fa9125d7b59cb44
denwritescode <suprundenis@gmail.com>
2026-03-15 17:43:19 +0300

    + creating theme directory structure and updating design plan

---
+++ composeApp/src/commonMain/kotlin/ru/den/writes/code/ui/theme/Color.kt
@@ -0,0 +1,5 @@
+package ru.den.writes.code.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Здесь мы определим корпоративные цвета в Этапе 2
---
+++ composeApp/src/commonMain/kotlin/ru/den/writes/code/ui/theme/Theme.kt
@@ -0,0 +1,9 @@
+package ru.den.writes.code.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+// Здесь мы соберем AppTheme в Этапе 4
