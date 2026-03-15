fe2610b8635f7b610ccfada8f91bbec1ced258c8
denwritescode <suprundenis@gmail.com>
2026-03-15 16:39:43 +0300

    + adding Koin DI module and updating integration plan

---
+++ composeApp/src/commonMain/kotlin/ru/den/writes/code/di/AppModule.kt
@@ -0,0 +1,25 @@
+package ru.den.writes.code.di
+
+import io.ktor.client.HttpClient
+import org.koin.core.context.startKoin
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.module
+import ru.den.writes.code.ServerStatusViewModel
+import ru.den.writes.code.network.NetworkMonitor
+import ru.den.writes.code.ui.main.MainViewModel
+
+val appModule = module {
+    // Сеть
+    single { HttpClient() }
+    single { NetworkMonitor(get()) }
+
+    // ViewModels
+    viewModelOf(::ServerStatusViewModel)
+    viewModelOf(::MainViewModel)
+}
+
+fun initKoin() {
+    startKoin {
+        modules(appModule)
+    }
+}
