4cdd9dc76896ec5fe6542f7d94fe1859c9f80dac
denwritescode <suprundenis@gmail.com>
2026-03-15 16:19:48 +0300

    + MainScreen integrated with VM into App

--- composeApp/src/commonMain/kotlin/ru/den/writes/code/App.kt
+++ composeApp/src/commonMain/kotlin/ru/den/writes/code/App.kt
@@ -1,34 +1,21 @@
 package ru.den.writes.code
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import corporatetasktracker.composeapp.generated.resources.Res
-import corporatetasktracker.composeapp.generated.resources.compose_multiplatform
 import io.ktor.client.HttpClient
-import org.jetbrains.compose.resources.painterResource
 import ru.den.writes.code.network.NetworkMonitor
 import ru.den.writes.code.ui.components.ServerStatusIndicator
+import ru.den.writes.code.ui.main.MainScreen
+import ru.den.writes.code.ui.main.MainViewModel
 
 val httpClient = HttpClient()
 val networkMonitor = NetworkMonitor(httpClient)
 
-// Инициализируем ViewModel глобально (пока без DI).
-// В будущем это лучше делать через Koin или другие DI-фреймворки.
+// Инициализируем ViewModels глобально (пока без DI).
 val serverStatusViewModel = ServerStatusViewModel(networkMonitor)
+val mainViewModel = MainViewModel()
 
 @Composable
 @Preview
@@ -39,29 +26,10 @@ fun App() {
                 ServerStatusIndicator(viewModel = serverStatusViewModel)
             }
         ) { paddingValues ->
-            var showContent by remember { mutableStateOf(false) }
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(paddingValues)
-                    .background(MaterialTheme.colorScheme.primaryContainer)
-                    .safeContentPadding(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Button(onClick = { showContent = !showContent }) {
-                    Text("Click me!")
-                }
-                AnimatedVisibility(showContent) {
-                    val greeting = remember { Greeting().greet() }
-                    Column(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                    ) {
-                        Image(painterResource(Res.drawable.compose_multiplatform), null)
-                        Text("Compose: $greeting")
-                    }
-                }
-            }
+            MainScreen(
+                viewModel = mainViewModel,
+                paddingValues = paddingValues
+            )
         }
     }
 }
---
+++ composeApp/src/commonMain/kotlin/ru/den/writes/code/ui/main/MainScreen.kt
@@ -0,0 +1,51 @@
+package ru.den.writes.code.ui.main
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import corporatetasktracker.composeapp.generated.resources.Res
+import corporatetasktracker.composeapp.generated.resources.compose_multiplatform
+import org.jetbrains.compose.resources.painterResource
+import ru.den.writes.code.Greeting
+
+@Composable
+fun MainScreen(
+    viewModel: MainViewModel,
+    paddingValues: PaddingValues = PaddingValues()
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues)
+            .background(MaterialTheme.colorScheme.primaryContainer)
+            .safeContentPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Button(onClick = { viewModel.toggleContent() }) {
+            Text("Click me!")
+        }
+        AnimatedVisibility(viewModel.showContent) {
+            val greeting = remember { Greeting().greet() }
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Image(painterResource(Res.drawable.compose_multiplatform), null)
+                Text("Compose: $greeting")
+            }
+        }
+    }
+}
