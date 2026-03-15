d45127e72627b3a79bc8def58df48cdc4a06aabc
denwritescode <suprundenis@gmail.com>
2026-03-15 16:07:45 +0300

    + ServerStatusViewModel created

---
+++ composeApp/src/commonMain/kotlin/ru/den/writes/code/ServerStatusViewModel.kt
@@ -0,0 +1,21 @@
+package ru.den.writes.code
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import ru.den.writes.code.network.NetworkMonitor
+import ru.den.writes.code.network.ServerStatus
+
+class ServerStatusViewModel(
+    networkMonitor: NetworkMonitor
+) : ViewModel() {
+
+    val status: StateFlow<ServerStatus> = networkMonitor.observeStatus()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = ServerStatus.CONNECTING
+        )
+}
